69cbe2cc161884dd4349378035d0cc11ec06903e
mattm3192 <mattm3192@gail.com>
2025-11-20 23:18:46 -0600

    CH5, L9: Completed, added ability to get all chirps.

---
+++ handler_chirps_get.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+)
+
+func (cfg *apiConfig) handlerChirpsGet(w http.ResponseWriter, r *http.Request) {
+	dbChirps, err := cfg.db.GetChirps(r.Context())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not retrieve chirps from database.", err)
+		return
+	}
+	chirps := []Chirp{}
+	for _, dbChirp := range dbChirps {
+		chirp := Chirp{
+			ID:        dbChirp.ID,
+			CreatedAt: dbChirp.CreatedAt,
+			UpdatedAt: dbChirp.UpdatedAt,
+			Body:      dbChirp.Body,
+			UserID:    dbChirp.UserID,
+		}
+		chirps = append(chirps, chirp)
+	}
+
+	respondWithJSON(w, http.StatusOK, chirps)
+}
